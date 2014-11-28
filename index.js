e3de45dfd2044b33c31f85f517833b14868081a2
Small Helm LLC <dev@smallhelm.com>
2014-11-28 16:20:12 -0700

    supporting basic CRUD

--- index.js
+++ index.js
@@ -7,47 +7,97 @@
 		ENTITY_WHARF = definition();//<script>
 	}
 }(function(){
+
+	var escaper = "$:?:$";
+
+	var toValIndex = function(v){
+		if(typeof v === 'object'){
+			return escaper + 'object';
+		}else if(typeof v === 'function'){
+			return escaper + 'function';
+		}
+		return v;
+	};
+
+	var isMap = function(o){
+		return !!o && typeof o === 'object' && o.constructor === Object;
+	};
+	var keys = Object.keys;
+	var has = function(o, attr){
+		return o.hasOwnProperty(attr);
+	};
+
+	var appendToIndex = function(index, attr, id){
+		if(!has(index, attr)){
+			index[attr] = {};
+		}
+		index[attr][id] = true;
+	};
+
+	var removeFromIndex = function(index, attr, id){
+		if(has(index, attr)){
+			delete index[attr][id];
+		}
+	};
+
 	return function(){
 		var nextID = (function(){
 			var last = 0;
 			return function(){
-				return ++last;
+				last++;
+				return last + "";//id's are strings
 			};
 		}());
 
-		var entities = {};
+		var e_index = {};
+		var a_index = {};
+		var v_index = {};
 
-		var attribute_index = {};
-		var value_index = {};
+		var has_e = function(id){
+			return has(e_index, id);
+		};
 
+		var set = function(id, data){ 
+			if(!has_e(id) || !isMap(data)){
+				return false;
+			}
+			keys(data).forEach(function(attr){
+				var val = data[attr]
+				e_index[id][attr] = val;
 
-		var set= function(id, data){ 
-			//TODO check if it's a good id first
-			//TODO extend entity[id] with the data
-
-			//TODO set indexes
+				appendToIndex(a_index, attr, id);
+				appendToIndex(v_index, toValIndex(val), id);
+			});
+			return true;
 		};
 
 		return {
 			get: function(id){
-				return entities[id];
+				return has_e(id) ? e_index[id] : null;
 			},
-				add: function(data){
-					var id = nextID();
-					entities[id] = {};
-					set(id, data);
-					return id;
-				},
-				set: set,
-				remove: function(id){
-					//TODO check if it's a good id first
-					delete entities[id];
-					//TODO remove indexes
-				},
-
-				q: function(){
-					//TODO queries
+			add: function(data){
+				var id = nextID();
+				e_index[id] = {};
+				set(id, data);
+				return id;
+			},
+			set: set,
+			remove: function(id){
+				if(!has_e(id)){
+					return false;
 				}
+				keys(e_index[id]).forEach(function(attr){
+					removeFromIndex(a_index, attr, id);
+					removeFromIndex(v_index, toValIndex(e_index[id][attr]), id);
+				});
+				delete e_index[id];
+				return true;
+			},
+
+			q: function(){
+				//TODO queries
+				return keys(e_index);
+			}
 		};
 	};
 }));
--- index.test.js
+++ index.test.js
@@ -1,5 +1,22 @@
+var Warf = require("./index");
+
 describe("entity-warf", function(){
-	it("should have tests", function(){
-		expect(1).toBe(1);
+	it("should do basic CRUD", function(){
+		var db = Warf();
+
+		var id = db.add({name: "bob", age: 50});
+		expect(typeof id).toEqual("string");
+		expect(db.q()).toEqual([id]);
+		expect(db.get(id)).toEqual({name: "bob", age: 50});
+
+		expect(db.set(id, {name: "bill", height: 6})).toBe(true);
+		expect(db.set(id, "jim")).toBe(false);
+		expect(db.set(id, ["jim"])).toBe(false);
+		expect(db.get(id)).toEqual({name: "bill", age: 50, height: 6});
+
+		expect(db.remove(id)).toBe(true);
+		expect(db.get(id)).toBe(null);
+		expect(db.remove(id)).toBe(false);
+		expect(db.set(id, {name: "jim"})).toBe(false);
 	});
 });
